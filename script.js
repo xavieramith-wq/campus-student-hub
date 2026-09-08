3aac7fb03b21c2cb78f5c00edd2b896b47d5dbc0
Xavier Amith J <xavier.amith@mca.christuniversity.in>
2026-09-08 08:50:34 +0530

    Add function to display student details

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function showStudentDetails() {
 
+    const details = document.getElementById("studentDetails");
+
+    details.innerHTML = `
+        <hr>
+        <h3>Profile Information</h3>
+        <p><strong>Department:</strong> Computer Applications</p>
+        <p><strong>Year:</strong> Second Year</p>
+        <p><strong>Status:</strong> Active Student</p>
+    `;
+}
